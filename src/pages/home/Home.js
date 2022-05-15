0fb9c7538ce2e16f05be73c21960a494d63b046d
Prince Thind <Princethind799@gmail.com>
2022-05-15 12:20:33 +0530

    add home screen animation

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import DashBoardCard from "./components/DashboardCard";
 import Stats from "./components/Stats";
 import Box from "@mui/material/Box";
+import Grow from "@mui/material/Grow";
 import { useSelector } from "react-redux";
 import { selectAllBooks } from "../../features/books/booksSlice";
 
@@ -10,10 +11,12 @@ function Home() {
   const readBooks = books.filter((b) => b.read).length;
 
   return (
-    <Box>
-      <DashBoardCard />
-      <Stats totalBooks={totalBooks} readBooks={readBooks} />
-    </Box>
+    <Grow in>
+      <Box>
+        <DashBoardCard />
+        <Stats totalBooks={totalBooks} readBooks={readBooks} />
+      </Box>
+    </Grow>
   );
 }
 
