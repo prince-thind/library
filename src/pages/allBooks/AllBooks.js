b6efdb5047275ca11f33130964eb564eaa06f746
Prince Thind <princethind799@gmail.com>
2022-06-15 17:15:59 +0530

    implement empty book screen display

--- src/pages/allBooks/AllBooks.js
+++ src/pages/allBooks/AllBooks.js
@@ -1,11 +1,22 @@
 import Grid from "@mui/material/Grid";
 import Grow from "@mui/material/Grow";
+import Typography from "@mui/material/Typography";
 import { useSelector } from "react-redux";
 import { selectAllBooks } from "../../features/books/booksSlice";
 import BookCard from "./components/BookCard";
 
 function AllBooks() {
   const books = useSelector(selectAllBooks);
+
+  if (books.length === 0) {
+    return (
+      <Typography variant="h2" component="h2">
+        No Book Found
+      </Typography>
+    );
+  }
+
+
   return (
     <Grow in>
       <Grid container spacing={2}>
