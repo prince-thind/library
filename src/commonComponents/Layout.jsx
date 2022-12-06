0cbd49e229d2744c4049751d3d9556ea114cea35
Prince Thind <princethind799@gmail.com>
2022-12-06 15:35:16 +0530

    migrate from CRA to vite

--- src/commonComponents/Layout.jsx
+++ src/commonComponents/Layout.jsx
@@ -1,6 +1,6 @@
 import { Box, Container } from "@mui/material";
 import Header from "./Header";
-import Footer from ".//Footer";
+import Footer from "./Footer";
 
 import { Outlet } from "react-router-dom";
 
